11283c96fc9104604569393a8673e3f8c2f5a26b
LANJAMIRANTSOA <161474996+LANJAMIRANTSOA@users.noreply.github.com>
2024-02-29 09:23:22 +0100

    Add files via upload

---
+++ php1/index.php
@@ -0,0 +1,23 @@
+<?php
+echo "Hello PHP, nous sommes le" .date("d-m-y");
+?>
+
+<!-- <?php
+// Début du code PHP
+
+// Définition de la date et de l'heure actuelle
+$date_actuelle = date("Y-m-d");
+$heure_actuelle = date("H");
+
+// Affichage du message avec la date et le moment de la journée
+echo "HELLO PHP, nous sommes le $date_actuelle ";
+
+// Vérification si c'est le matin (avant midi)
+if ($heure_actuelle < 12) {
+    echo "bon matin";
+} else { // Sinon, c'est l'après-midi
+    echo "bon après-midi";
+}
+
+// Fin du code PHP
+?> -->
